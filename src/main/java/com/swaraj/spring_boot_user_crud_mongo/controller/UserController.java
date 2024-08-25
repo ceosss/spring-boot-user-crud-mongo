3fb81979504cb8ff8bda59106bf7be3318dd2f6b
Swaraj <sswarajsamant@gmail.com>
2024-08-25 18:49:49 +0530

    feat: get user controller

--- src/main/java/com/swaraj/spring_boot_user_crud_mongo/controller/UserController.java
+++ src/main/java/com/swaraj/spring_boot_user_crud_mongo/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.swaraj.spring_boot_user_crud_mongo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
@@ -15,5 +15,10 @@ public class UserController {
     public List<String> getUsers() {
         return List.of("swaraj", "reo", "dev");
     }
-    
+
+    @GetMapping("/{id}")
+    public String getUser(@PathVariable String id) {
+        return new String("swaraj" + " " + id);
+    }
+
 }
