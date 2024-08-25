1fcbe1d3dde2e44bcc5a4bb700668aa72960cbbe
Swaraj Samant Singh <swarajsamantsingh@Swarajs-MacBook-Pro.local>
2024-08-25 18:32:31 +0530

    feat: user model

---
+++ src/main/java/com/swaraj/spring_boot_user_crud_mongo/model/User.java
@@ -0,0 +1,11 @@
+package com.swaraj.spring_boot_user_crud_mongo.model;
+
+import java.time.LocalDate;
+
+public class User {
+    long id;
+    String name;
+    int age;
+    String gender;
+    LocalDate dob;
+}
