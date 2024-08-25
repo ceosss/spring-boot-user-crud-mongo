6de5fa834905c75685b18442d351c5d71d4e9381
Swaraj <sswarajsamant@gmail.com>
2024-08-25 19:00:54 +0530

    feat: user model constructor, getters and setters

--- src/main/java/com/swaraj/spring_boot_user_crud_mongo/model/User.java
+++ src/main/java/com/swaraj/spring_boot_user_crud_mongo/model/User.java
@@ -8,4 +8,54 @@ public class User {
     int age;
     String gender;
     LocalDate dob;
+
+    public User() {
+    }
+
+    public User(String name, int age, String gender, LocalDate dob) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.dob = dob;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
 }
