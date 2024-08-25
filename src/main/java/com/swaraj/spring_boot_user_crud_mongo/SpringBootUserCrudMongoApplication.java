d017577e13ab6204061b20bda785f9e00d159650
Swaraj Samant Singh <swarajsamantsingh@Swarajs-MacBook-Pro.local>
2024-08-25 18:13:17 +0530

    INITIAL COMMIT

---
+++ src/main/java/com/swaraj/spring_boot_user_crud_mongo/SpringBootUserCrudMongoApplication.java
@@ -0,0 +1,13 @@
+package com.swaraj.spring_boot_user_crud_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserCrudMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUserCrudMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swaraj/spring_boot_user_crud_mongo/SpringBootUserCrudMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swaraj.spring_boot_user_crud_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUserCrudMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
